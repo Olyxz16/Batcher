8ab8ee4cc33b549a4469f717ce5291bf21c2f0bd
Olyxz16 <cedric.sazos@tutanota.com>
2023-02-26 17:59:28 +0100

    refactored loading function variable names

--- src/loader/loader.js
+++ src/loader/loader.js
@@ -2,24 +2,29 @@ const { downloadItems } = require("./downloader.js");
 
 async function loadItems(options) {
     return new Promise((resolve, reject) => {
-      loadAux(options, [], [], [], resolve);
+      load(options, [], [], [], resolve);
     });
 }
 
-async function loadAux(options, fulfilled, rejected, processing, resolve) {
+async function load(options, fulfilled, rejected, processing, resolve) {
   if(Object.keys(options["items"]).length === 0 && processing.length === 0) {
     resolve(mapResult(fulfilled, rejected));
     return;
   }
   for(let tag in options.items) {
     let item = options.items[tag];
-    
-    if(item["depends"] != undefined && item["depends"].some(v => rejected.includes(v))) {
+
+    item["depends"] ||= [];
+    let itemHasDependencies = item["depends"].length > 0;
+    let itemHasRejectedDependencies = item["depends"].some(v => rejected.includes(v));
+    let itemHasUnprocessedDependencies = !item["depends"].every(v => fulfilled.includes(v));
+
+    if(itemHasDependencies && itemHasRejectedDependencies) {
       delete options["items"][tag];
       rejected.push(tag);
-      loadAux(options, fulfilled, rejected, processing, resolve);
+      load(options, fulfilled, rejected, processing, resolve);
     }
-    if(item["depends"] != undefined && !item["depends"].every(v => fulfilled.includes(v))) {
+    if(itemHasDependencies && itemHasUnprocessedDependencies) {
       continue;
     }
     delete options["items"][tag];
@@ -28,12 +33,12 @@ async function loadAux(options, fulfilled, rejected, processing, resolve) {
     .then((result) => {
       fulfilled.push(tag);
       processing.splice(processing.indexOf(tag), 1);
-      loadAux(options, fulfilled, rejected, processing, resolve)
+      load(options, fulfilled, rejected, processing, resolve)
     })
     .catch((error) => {
       rejected.push(tag);
       processing.splice(processing.indexOf(tag), 1);
-      loadAux(options, fulfilled, rejected, processing, resolve);
+      load(options, fulfilled, rejected, processing, resolve);
     });
   }
 }
